f975904272b48a5cd86fee9abd8a9761fba68eb7
John Mamby <john.mamby@keyrus.com>
2019-06-12 15:30:49 -0500

    [CHALLENGE I] Removed unused import

--- src/keyruswarmupstorefront/web/src/br/com/keyrus/warmup/storefront/controllers/pages/CartPageController.java
+++ src/keyruswarmupstorefront/web/src/br/com/keyrus/warmup/storefront/controllers/pages/CartPageController.java
@@ -62,7 +62,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import net.sf.saxon.exslt.Random;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
